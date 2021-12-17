ec1cb517d927c5969a30553c046c3c8fdef649c0
lymchgmk <lymchgmk@gmail.com>
2021-12-18 00:32:55 +0900

    Chore: 코인 출력 순서 고정

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -20,12 +20,11 @@ public class OutputView {
 
     public static void printCurrUserMoney(Integer currUserMoney) {
         System.out.println(USER_MONEY + currUserMoney + KRW);
-        System.out.println();
     }
 
     public static void printChangeCoins(Map<Integer, Integer> changeCoins){
         System.out.println(CHANGES_MSG);
-        for (int coinValue : changeCoins.keySet()) {
+        for (int coinValue : Coin.getAmountsList()) {
             System.out.println(
                 coinValue + KRW + COINS_MSG_DELIMETER + changeCoins.get(coinValue) + EA
             );
